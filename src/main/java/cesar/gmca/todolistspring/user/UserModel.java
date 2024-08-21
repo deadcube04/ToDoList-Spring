05ed8ac09b43a92151b787115df88b3f40c53345
Gabriel Albuquerque <gabrielmca2004@gmail.com>
2024-08-21 19:58:48 -0300

    adding getters and setters

---
+++ src/main/java/cesar/gmca/todolistspring/user/UserModel.java
@@ -0,0 +1,31 @@
+package cesar.gmca.todolistspring.user;
+
+public class UserModel {
+
+    private String username, name, password;
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+    //getters e setters
