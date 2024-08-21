a71e74835dce1e24e716a0ac3109e5bc3ec78ea2
Gabriel Albuquerque <gabrielmca2004@gmail.com>
2024-08-21 20:08:11 -0300

    fixing setters and getters

--- src/main/java/cesar/gmca/todolistspring/user/UserModel.java
+++ src/main/java/cesar/gmca/todolistspring/user/UserModel.java
@@ -1,31 +1,11 @@
 package cesar.gmca.todolistspring.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
 
     private String username, name, password;
 
-    public void setUsername(String username){
-        this.username = username;
-    }
-
-    public String getUsername(){
-        return username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
     //getters e setters
